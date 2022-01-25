a0c6b6ef78fdf8e23e20ddc6cc56e197ea2697b6
YesCoops <coopandmgc@gmail.com>
2022-01-25 08:41:40 +0000

    Remove gspread and oauth for google sheets api

--- run.py
+++ run.py
@@ -1,24 +1,6 @@
 
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('starcommand')
-
-usernames = SHEET.worksheet('usernames')                           
-
-user_data = usernames.get_all_values()
-
-print(user_data)
 
 SHIP_LENGTH = [2,3,3,4,5]
 PLAYER_BOARD = [[" "] * 10 for i in range(10)]
