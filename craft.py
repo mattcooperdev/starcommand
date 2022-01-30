4637f10d270d74ae9ad80e4dd1e5924225256bfe
YesCoops <coopandmgc@gmail.com>
2022-01-30 18:03:42 +0000

    Create Craft Class and relevant subclasses

---
+++ craft.py
@@ -0,0 +1,37 @@
+class Craft:
+    """
+    Creates properties of class that are shared 
+    by the subclasses to initialise a craft object
+    """
+
+    def __init__(self, start_point, direction, coordinates):
+        self.start_point = start_point
+        self.direction = direction
+        self.coordinates = coordinates
+        self.damaged_tiles = []
+        self.is_destroyed = False
+
+class AsteroidDodger(Craft):
+    name = "Asteroid Dodger"
+    length = 5
+    id_list = ["A"] * length
+
+class Blaster(Craft):
+    name = "Blaster"
+    length = 4
+    id_list = ["B"] * length
+
+class CometKiller(Craft):
+    name = "Comet Killer"
+    length = 3
+    id_list = ["C"] * length
+
+class StarChaser(Craft):
+    name = "Star Chaser"
+    length = 3
+    id_list = ["S"] * length
+
+class Destroyer(Craft):
+    name = "Destroyer"
+    length = 2
+    id_list = ["D"] * length
